620af1cb064cf75e415cdc14b47130f8fd955a8a
Mike <69970540+mikerb95@users.noreply.github.com>
2026-02-27 07:59:35 -0500

    feat: Initialize SlideHub project with Spring Boot
    - Added Maven Wrapper scripts (mvnw and mvnw.cmd) for easier project setup.
    - Created pom.xml with Spring Boot dependencies and configurations.
    - Implemented main application class (SlideHubApplication) to bootstrap the Spring Boot application.
    - Added application.properties to define application name.
    - Created basic test class (SlideHubApplicationTests) to verify application context loading.

---
+++ src/main/java/com/brixo/SlideHub/SlideHubApplication.java
@@ -0,0 +1,13 @@
+package com.brixo.SlideHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlideHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlideHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brixo/SlideHub/SlideHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brixo.SlideHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlideHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
